dc675d91e008f048419bfed9d617e8e91e393a3f
Joao Victor <victor.jvas1@gmail.com>
2023-08-07 18:54:54 -0300

    retrieving name

--- go-scraper.go
+++ go-scraper.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/gocolly/colly/v2"
 )
 
+type player struct {
+	Name     string
+	Position int
+	World    string
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	//_, file := os.Create("ranking.json")
 
-	_, file := os.Create("ranking.json")
+	//println(file)
 
-	println(file)
+	//var ranking []player
 
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting crystalline conflicts ranking")
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnHTML(".cc-ranking__result__name", func(h *colly.HTMLElement) {
+		println(h.ChildText("h3"))
+
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("scraping done!")
 	})
 
 	c.Visit("https://na.finalfantasyxiv.com/lodestone/ranking/crystallineconflict/")
